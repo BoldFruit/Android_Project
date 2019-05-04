d6e5b8c53c7c6c43c0970eadfea68fe8974d6455
BoldFruit <798382030@qq.com>
2019-05-04 18:00:51 +0800

    update

---
+++ SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.sharedpreferencestest;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button saveData = (Button) findViewById(R.id.save_data);
+        saveData.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View V){
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name", "Tom");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", false);
+                editor.apply();
+        }
+        });
+        Button restoreData = (Button) findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d(TAG, "name is" + name);
+                Log.d(TAG, "age is" + age);
+                Log.d(TAG, "married is" + married);
+            }
+        });
+    }
+}
